fd0a5ee4b801f55fa989e06a4d0ed0f85b5c198b
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-02-28 16:56:23 -0500

    add movie component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Movie from './Movie.js';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,11 +9,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Movie/>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
---
+++ src/Movie.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Movie extends Component {
+  render() {
+    return (
+      <div>
+        <h1>The Lord of the Rings</h1>
+        <h2> A Heartwarming Trilogy of Smalltown Compassion Meets Big City Ambition</h2>
+        <p> 4h 37min</p>
+      </div>
+    )
+  }
+}
+
+
+export default Movie;
